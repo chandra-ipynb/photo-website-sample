6ac8e86975f37e4fb981d5a8ad26929316600621
chandra-ipynb <chandra71996@gmail.com>
2026-09-26 10:35:01 +0530

    Update photography portfolio

--- site-config.js
+++ site-config.js
@@ -3,6 +3,6 @@ window.SITE_CONFIG = {
   "phone": "+919945690520",
   "displayPhone": "+91 99456 90520",
   "address": "06, 10th Main Jeevanbeema Nagar, Nagar Main Road, HAL 3rd Stage, Kodihalli, Bengaluru, Karnataka 560075",
-  "instagram": "https://www.instagram.com/jkphotography_hsr?igsi=MWcyaXY2dHo1ZW01cA==",
+  "instagram": "https://www.instagram.com",
   "whatsappMessage": "Hello, I would like to book a photography session."
 };
